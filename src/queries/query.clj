35a1877ffedbf7989917fc88c0895b6e44c6f07f
Lazar Miric <lazarmiric@outlook.com>
2021-07-22 09:41:55 +0200

    Added new api(customer,product), bug fix in domain

--- src/queries/query.clj
+++ src/queries/query.clj
@@ -4,15 +4,49 @@
               [korma.db :refer :all]
               [schema.core :as schema]
               [domain.customer :refer :all]
-              [domain.order :refer :all]
+              [domain.orders :refer :all]
               [domain.product :refer :all]
               [domain.product_detail :refer :all])
     (:import (java.security MessageDigest)))
 
 
-    ;; Customer
+    ; Customer
 
     (defentity customer)
 
     (defn get-customers[]
         (select customer))
+
+    (defn get-customer[CustomerID]
+       (first
+          (select customer
+            (where {:CustomerID [= CustomerID]} )
+             (limit 1))))
+
+   (defn add-customer [NewCustomer]
+                  ((def newCustomer (insert customer
+                    (values {
+                      :Name (get NewCustomer :Name)
+                      :LastName (get NewCustomer :LastName)
+                      :City (get NewCustomer :City)
+                      })
+                    ))
+                    (def newCustomerID (get newCustomer :generated_key))
+                    (get-customer newCustomerID)
+                  )
+                )
+
+  ; Product 
+
+  (defentity product)
+
+    (defn get-product [ProductID]
+         (first
+          (select product
+            (where {:ProductID [= ProductID]} )
+              (limit 1))))
+
+
+
+
+  
