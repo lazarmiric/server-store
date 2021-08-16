a477f88bcfed51f4544e216c3142a5e743a4e2c4
Lazar Miric <lazarmiric@outlook.com>
2021-08-16 14:47:27 +0200

    Updated sql file (Change in database)

--- resources/StoreDatabase.sql
+++ resources/StoreDatabase.sql
@@ -8,6 +8,8 @@
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
@@ -21,12 +23,12 @@ DROP TABLE IF EXISTS `customer`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `customer` (
-  `CustomerID` int NOT NULL,
+  `CustomerID` int NOT NULL AUTO_INCREMENT,
   `Name` varchar(45) DEFAULT NULL,
   `LastName` varchar(45) DEFAULT NULL,
-  `Ciry` varchar(45) DEFAULT NULL,
+  `City` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`CustomerID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -35,20 +37,20 @@ CREATE TABLE `customer` (
 
 LOCK TABLES `customer` WRITE;
 /*!40000 ALTER TABLE `customer` DISABLE KEYS */;
-INSERT INTO `customer` VALUES (1,'Lazar','Miric','Smederevo'),(2,'Pera','Peric','Nis'),(3,'Milos','Milosevic','Subotica'),(4,'Aca','Peric','Bor'),(5,'Mihajlo','Mihic','Arandjelovac');
+INSERT INTO `customer` VALUES (1,'Lazar','Miric','Smederevo'),(2,'Pera','Peric','Nis'),(3,'Milos','Milosevic','Subotica'),(4,'Aca','Peric','Bor'),(5,'Mihajlo','Mihic','Arandjelovac'),(11,'Lazar','Miric','Smederevo'),(12,'Lazar','Miric','Smederevo'),(13,'Nikola','Jokic','Sombor'),(14,'Nikola','Jokic','Sombor'),(18,'Nikola','Jokic','Sombor'),(19,'Nikola','Jokic','Sombor'),(20,'Nikola','Jokic','Sombor'),(21,'Nikola','Jokic','Sombor'),(22,'Nikola','Jokic','Sombor'),(23,'Nikola','Jokic','Sombor'),(24,'Nikola','Jokic','Sombor');
 /*!40000 ALTER TABLE `customer` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `order`
+-- Table structure for table `orders`
 --
 
-DROP TABLE IF EXISTS `order`;
+DROP TABLE IF EXISTS `orders`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `order` (
+CREATE TABLE `orders` (
   `OrderID` int NOT NULL AUTO_INCREMENT,
-  `TotalPayment` decimal(10,0) DEFAULT NULL,
+  `TotalPayment` double DEFAULT NULL,
   `CustomerID` int DEFAULT NULL,
   `ProductID` int DEFAULT NULL,
   `TotalOrderOfSameProduct` int DEFAULT NULL,
@@ -57,17 +59,17 @@ CREATE TABLE `order` (
   KEY `customer_id_fk_idx` (`CustomerID`),
   CONSTRAINT `customer_id_fk` FOREIGN KEY (`CustomerID`) REFERENCES `customer` (`CustomerID`),
   CONSTRAINT `product_id_fk` FOREIGN KEY (`ProductID`) REFERENCES `product` (`ProductID`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `order`
+-- Dumping data for table `orders`
 --
 
-LOCK TABLES `order` WRITE;
-/*!40000 ALTER TABLE `order` DISABLE KEYS */;
-INSERT INTO `order` VALUES (1,15000,1,1,20),(2,10000,2,5,10),(3,30000,3,4,50),(4,13000,4,6,20),(5,9000,5,2,10);
-/*!40000 ALTER TABLE `order` ENABLE KEYS */;
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (1,15000,1,1,20),(2,10000,2,5,10),(3,30000,3,4,50),(4,13000,4,6,20),(6,10000,1,1,10),(7,1000,1,1,10),(8,1000,1,1,10),(9,1000,1,1,10),(13,1000,1,1,10),(14,1000,1,1,10),(15,1000,5,3,50),(16,10000,1,1,10),(17,10000,1,1,10),(18,10000,1,1,10),(19,10000,1,1,10),(20,10000,1,1,10),(21,10000,1,1,10),(22,10000,1,1,10),(23,10000,1,1,10),(24,10000,1,1,10);
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -95,7 +97,7 @@ CREATE TABLE `product` (
 
 LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
-INSERT INTO `product` VALUES (1,'S10','Phone',1,750),(2,'11 Pro Max','Phone',2,900),(3,'S21653','Fridge',4,450),(4,'Blade','Phone',6,600),(5,'12 Pro Max','Phone',2,1000),(6,'K5632','Cooker',5,650),(7,'I43-21','TV',3,350),(8,'GT-42','Smart watch',6,150);
+INSERT INTO `product` VALUES (1,'S10','Phone',1,1500),(2,'11 Pro Max','Phone',2,900),(3,'S21653','Fridge',4,450),(4,'Blade','Phone',6,600),(5,'12 Pro Max','Phone',2,1000),(6,'K5632','Cooker',5,650),(8,'GT-42','Smart watch',6,100);
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -110,7 +112,7 @@ CREATE TABLE `product_detail` (
   `ProductDetailID` int NOT NULL,
   `Brand` varchar(45) NOT NULL,
   `CountryOfOrigin` varchar(45) DEFAULT NULL,
-  `YearsOfGarancy` int DEFAULT NULL, 
+  `YearsOfGarancy` int DEFAULT NULL,
   PRIMARY KEY (`ProductDetailID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -124,6 +126,7 @@ LOCK TABLES `product_detail` WRITE;
 INSERT INTO `product_detail` VALUES (1,'Samsung','China',3),(2,'Apple','China',3),(3,'Tesla','America',2),(4,'Midea','Italy',6),(5,'Gorenje','Slovenia',5),(6,'Huawei','China',2);
 /*!40000 ALTER TABLE `product_detail` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -133,4 +136,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed
+-- Dump completed on 2021-08-16 14:43:36
